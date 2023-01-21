4a6544704c9f115d8c17b88ebe0c86726ff6db7b
Choris <me@chorismate.dev>
2023-01-20 17:20:02 -0800

    algor.c: add empty functions

--- algor.c
+++ algor.c
@@ -11,3 +11,45 @@
  * with this software. If not, see
  * <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
+
+#include<inttypes.h>
+#include<stdio.h>
+
+/* Board
+ * 0 1 2
+ * 3 4 5
+ * 6 7 8
+ */
+
+/* Returns an pointer to an array of 8 uint8_t:
+ * 0: 0-1-2
+ * 1: 3-4-5
+ * 2: 6-7-8
+ * 3: 0-3-6
+ * 4: 1-4-7
+ * 5: 2-5-8
+ * 6: 0-4-8
+ * 7: 2-4-6
+ * For each:
+ * 0: The line is empty.
+ * 1: The line is blocked (has at least 1 of each color).
+ * 2: The current player has 1 on that line.
+ * 3: The current player has 2 on that line.
+ * 4: The current player has a win on that line.
+ * 5: The opponent has 1 on that line.
+ * 6: The opponent has 2 on that line.
+ * 7: The opponent has a win on that line.
+ */
+uint8_t* get_aligns(uint8_t *state) {
+}
+
+/* Takes an pointer to an array of 9 uint8_t,
+ * returns best move in grid as a uint8_t
+ */
+uint8_t algor(uint8_t *state) {
+}
+
+/* Main */
+int main(void) {
+	return 0;
+}
